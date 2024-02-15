c317939e8a0630b5e4aab4074c1c07dd871d981d
Yussef Dalton <r0930164@student.thomasmore.be>
2024-02-15 11:42:33 +0100

    Add the database migrations

---
+++ database/migrations/2024_02_15_111342_create_noti_comps_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('noti_comps', function (Blueprint $table) {
+            $table->foreignId('notification_id')->nullable(false)->onDelete('cascade');
+            $table->foreignId('competition_id')->nullable(false)->onDelete('cascade');
+            $table->integer('interval_exception')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('noti_comps');
+    }
+};
