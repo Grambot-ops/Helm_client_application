780d7df7c28c8ae6b13ae3578fa69ccd38ad0c47
Katoo_Roofthooft <r0929442@student.thomasmore.be>
2024-02-21 20:28:50 +0100

    Footer made

---
+++ resources/views/components/Layout/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="w-full px-4 py-6 text-sm border-t flex flex-wrap justify-between items-center text-white" style="background-color: #f26130">
+    <div class="flex flex-wrap justify-between w-full md:w-auto">
+        <div class="mb-1 text-white">
+            Thomas More Competition Platform - © {{ date('Y') }}
+        </div>
+    </div>
+    <div class="flex flex-wrap w-full md:w-auto text-white">
+        <div class="mr-10 mb-2 md:mb-0">
+            <h5 class="text-lg font-bold mb-2">Contact Us</h5>
+            <p>Email: info.geel@thomasmore.be</p>
+            <p>Phone: +32 014 72 13 00</p>
+        </div>
+        <div class="mr-10 mb-2 md:-0">
+            <h5 class="text-lg font-bold mb-2">Address</h5>
+            <p>Kleinhoefstraat 4</p>
+            <p>2440 Geel</p>
+        </div>
+    </div>
+</footer>
