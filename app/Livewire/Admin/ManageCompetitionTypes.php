e59429760f819bed0481d62c0888f82685f31878
Katoo_Roofthooft <r0929442@student.thomasmore.be>
2024-03-14 00:04:39 +0100

    manage t types update

---
+++ app/Livewire/Admin/ManageCompetitionTypes.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use App\Livewire\Forms\TypeForm;
+use App\Models\Competition;
+use App\Models\CompetitionType;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ManageCompetitionTypes extends Component
+{
+    use WithPagination;
+
+    public $orderBy = 'name';
+    public $orderAsc = true;
+    public $search;
+    public $perPage = 5;
+    public $newType;
+    public $showModal = false;
+    public TypeForm $form;
+
+    public function updated($propertyName)
+    {
+        if (in_array($propertyName, ['search', 'perPage']))
+            $this->resetPage();
+    }
+    public function editType(CompetitionType $type)
+    {
+        $this->resetErrorBag();
+        $this->form->fill($type);
+        $this->showModal = true;
+    }
+    public function newType()
+    {
+        $this->form->reset();
+        $this->resetErrorBag();
+        $this->showModal = true;
+    }
+
+    public function deleteType(CompetitionType $type)
+    {
+        $this->form->delete($type);
+        $this->dispatch('swal:toast', [
+            'background' => 'success',
+            'html' => "The type <b><i>{$type->name}</i></b> has been deleted",
+            'icon' => 'success',
+        ]);
+    }
+
+    public function createType()
+    {
+        $validatedData = $this->validate([
+            'newType' => 'required|unique:competition_types,name',
+        ], [
+            'newType.unique' => 'The type name has already been taken.',
+        ]);
+
+        CompetitionType::create([
+            'name' => trim($validatedData['newType'])
+        ]);
+
+        $this->dispatch('swal:toast', [
+            'background' => 'success',
+            'html' => "The type <b><i>{$this->form->name}</i></b> has been added",
+            'icon' => 'success',
+        ]);
+    }
+    public function updateType(CompetitionType $type)
+    {
+        $existingType = CompetitionType::where('name', $this->form->name)
+            ->where('id', '!=', $type->id)
+            ->exists();
+
+        if ($existingType) {
+            $this->addError('form.name', 'That type name already exists.');
+            return;
+        }
+        $this->form->update($type);
+        $this->showModal = false;
+        $this->dispatch('swal:toast', [
+            'background' => 'success',
+            'html' => "The record <b><i>{$this->form->name}</i></b> has been updated",
+            'icon' => 'success',
+        ]);
+    }
+
+    #[Layout('layouts.tmcp', ['title' => 'types', 'description' => 'Manage the types of your competitions',])]
+    public function render()
+    {
+        $query = CompetitionType::orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc');
+
+        if ($this->search) {
+            $query->where('name', 'like', '%' . $this->search . '%');
+        }
+
+        $types = $query->paginate($this->perPage);
+
+        return view('livewire.admin.manage-competition-types', compact('types'));
+    }
+
+    public function resort($column)
+    {
+        $this->orderBy === $column ?
+            $this->orderAsc = !$this->orderAsc :
+            $this->orderAsc = true;
+        $this->orderBy = $column;
+    }
+}
