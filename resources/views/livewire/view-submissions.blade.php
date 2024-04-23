709e1f778ef1b6f2b2270168f81bff5867566922
Stef <r0739242@student.thomasmore.be>
2024-04-23 09:52:04 +0200

    Check if the user is organiser to be able to delete submissions

--- resources/views/livewire/view-submissions.blade.php
+++ resources/views/livewire/view-submissions.blade.php
@@ -15,7 +15,7 @@
                                         <x-button wire:click="openInfo({{$submission}})" class="bg-transparent hover:bg-transparent enabled:bg-transparent focus:bg-transparent px-0.5 mx-05" href="#">
                                             <x-phosphor-info class="inline-block w-5 h-5 text-blue-400"/>
                                         </x-button>
-                                        @if(auth()->user()->admin||auth()->user()->user_roles()->where('role_id', 3)->exists())
+                                        @if(auth()->user()->admin||auth()->user()->id==$competition->organiser_id)
                                         <x-button wire:click="openDelete({{$submission}})" class="bg-transparent hover:bg-transparent active:bg-transparent enabled:bg-transparent focus:bg-transparent px-0.5 mx-0.5" href="#">
                                             <x-phosphor-trash class="inline-block w-5 h-5 text-red-600"/>
                                         </x-button>
