d469a8b246b79a42393230aa1481ebd097c9c6e9
Stef <r0739242@student.thomasmore.be>
2024-04-25 11:02:57 +0200

    update view of the sent emails

--- resources/views/emails/announcement-sent.blade.php
+++ resources/views/emails/announcement-sent.blade.php
@@ -1,5 +1,9 @@
 <div>
     <title>{{$details['subject']}}</title>
+    <h1>{{$details['subject']}}</h1>
     <p>{{$details['message']}}</p>
+    <br>
+    <p>This email has been automatically sent using our system. If you want to respond, email the competition owner for
+        your questions.</p>
 </div>
 
