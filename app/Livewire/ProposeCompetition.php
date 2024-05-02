ae0c196ee8782fb2ffe37ecf2421ea1b30dcec9a
NielsVanNerum <r0894354@student.thomasmore.be>
2024-05-02 09:19:53 +0200

    small changes

--- app/Livewire/ProposeCompetition.php
+++ app/Livewire/ProposeCompetition.php
@@ -6,23 +6,28 @@ use App\Livewire\Forms\CompetitionForm;
 use App\Models\CompetitionCategory;
 use App\Models\CompetitionType;
 use Livewire\Attributes\Layout;
+use Livewire\WithFileUploads;
 use Livewire\Component;
 
 class ProposeCompetition extends Component
 {
+    use WithFileUploads;
+
     public CompetitionForm $form;
+
+    public $termsOfAgreement = false;
 
     public function createCompetition()
     {
-       $this->form->create();
-       $this->dispatch('swal:toast', [
+        $this->form->create();
+        $this->dispatch('swal:toast', [
             'background' => 'success',
             'html' => "The competition <b><i>{$this->form->title}</i></b> has been proposed",
             'icon' => 'success',
        ]);
     }
 
-    #[Layout('layouts.tmcp', ['title' => 'Competitions', 'description' => 'Thomas More Competition Platform'])]
+    #[Layout('layouts.tmcp', ['title' => 'Propose Competition', 'description' => 'Thomas More Competition Platform'])]
     public function render()
     {
         $competition_types = CompetitionType::orderBy('name')->get();
