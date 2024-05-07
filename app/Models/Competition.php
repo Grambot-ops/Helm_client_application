cae67d709708f5cc7d7dddd1d05a3dc38e11480b
Yussef Dalton <r0930164@student.thomasmore.be>
2024-05-07 11:03:54 +0200

    propose-competition: add ability to pick filetype

--- app/Models/Competition.php
+++ app/Models/Competition.php
@@ -14,6 +14,38 @@ class Competition extends Model
     protected $appends = ['closed', 'open', 'vote', 'upload', 'is_liked'];
     protected $guarded = [];
 
+    // this is SEVERELY brain-damaged but I cannot otherwise
+    private static $filetypes = [
+        'image' => 'Image (jpeg, png, gif)',
+        'video' => 'Video (mp4)',
+        'audio' => 'Audio (mp3)',
+        'document' => 'Documents (pdf, docx, xlsx, pptx, txt)',
+        'archive' => 'Archive (zip, tar.gz, 7z, tar.xz)'
+    ];
+
+    private static $file_formats = [
+        'image' => 'jpeg,jpg,png,gif',
+        'video' => 'mp4',
+        'audio' => 'mp3',
+        'document' => 'pdf,docx,xlsx,pptx,txt',
+        'archive' => 'zip,tar.gz,7z,tar.xz',
+    ];
+
+    public static function getFileTypes()
+    {
+        return static::$filetypes;
+    }
+
+    public static function fileTypesToFormats(string $_filetypes): string
+    {
+        $formats = '';
+        foreach(explode(',', $_filetypes) as $filetype) {
+            $formats .= static::$file_formats[$filetype];
+        }
+        $formats = rtrim($formats, ',');
+        return $formats;
+    }
+
     public function getClosedAttribute(): bool
     {
         return $this->attributes['end_date'] < Carbon::now();
