957153460e166defa090780ec0a11884631a5448
NielsVanNerum <r0894354@student.thomasmore.be>
2024-05-07 11:20:15 +0200

    edit competitions finished

--- app/Livewire/ProposeCompetition.php
+++ app/Livewire/ProposeCompetition.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use App\Livewire\Forms\CompetitionForm;
+use App\Models\Competition;
 use App\Models\CompetitionCategory;
 use App\Models\CompetitionType;
+use Illuminate\Http\Request;
 use Livewire\Attributes\Layout;
 use Livewire\WithFileUploads;
 use Livewire\Component;
@@ -14,6 +16,7 @@ class ProposeCompetition extends Component
     use WithFileUploads;
 
     public CompetitionForm $form;
+    public $competition;
 
     public $termsOfAgreement = false;
 
@@ -27,11 +30,30 @@ class ProposeCompetition extends Component
        ]);
     }
 
-    #[Layout('layouts.tmcp', ['title' => 'Propose Competition', 'description' => 'Thomas More Competition Platform'])]
-    public function render()
+    public function updateRecord(Competition $competition)
     {
-        $this->form->number_of_votes_allowed = 1;
-        $this->form->number_of_uploads = 3;
+        $this->form->update($competition);
+        $this->dispatch('swal:toast', [
+            'background' => 'success',
+            'html' => "The competition <b><i>{$this->form->title}</i></b> has been edited",
+            'icon' => 'success',
+        ]);
+    }
+
+    #[Layout('layouts.tmcp', ['title' => 'Propose Competition', 'description' => 'Thomas More Competition Platform'])]
+    public function render(Request $request)
+    {
+        $id = $request->id;
+
+        $this->competition = Competition::where('id', $id ?? 0)->first();
+
+        if($this->competition) {
+            $this->resetErrorBag();
+            $this->form->fill($this->competition);
+        } else {
+            $this->form->number_of_votes_allowed = 1;
+            $this->form->number_of_uploads = 3;
+        }
         $competition_types = CompetitionType::orderBy('name')->get();
         $competition_categories = CompetitionCategory::orderBy('name')->get();
         return view('livewire.propose-competition', compact('competition_types', 'competition_categories'));
