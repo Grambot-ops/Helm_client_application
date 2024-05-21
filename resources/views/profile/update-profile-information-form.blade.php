7aebc4a3c1e6175b18ea510a2158806591db6f2f
NathanDhoore <r0916741@student.thomasmore.be>
2024-05-21 08:48:29 +0200

    remove pic fixed

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -46,7 +46,7 @@
                     {{ __('Select A New Photo') }}
                 </x-secondary-button>
 
-                @if ($this->user->profile_photo_path && $this->user->profile_photo_path !== 'assets/profile_pictures/default.jpg')
+                @if ($this->user->profile_photo_path !== '/assets/profile_pictures/default.jpg')
                     <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
                         {{ __('Remove Photo') }}
                     </x-secondary-button>
