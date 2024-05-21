8d1be354c23eb8bac1258187e26bf5c596ded0da
Katoo_Roofthooft <r0929442@student.thomasmore.be>
2024-05-21 19:51:53 +0200

    last fixes footer

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,4 +1,4 @@
-<x-tmcp-layout page_author='Yussef'>
+<x-tmcp-layout page_author='Yussef, Nathan'>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
